e177230fb446d95ed818490c2dc3e23341c35fe5
Mohammed Mamun <momamun@yahoo.com>
2020-08-07 23:03:44 -0400

    sign and http call

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,15 +10,32 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
+  signupRequestPayload: SignupRequestPayload;
   signupForm: FormGroup;
-  constructor() { }
+
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: ''
+    };
+  }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       username: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
     });
   }
 
+  signup(): void {
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
